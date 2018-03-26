5e42b571244f4ba132556a6d363d0f2d32e0b4b3
Nathan Young <nathanpmyoung@gmail.com>
2018-03-26 10:10:14 +0100

    first commit

---
+++ wp-content/themes/pizzaria/index.php
@@ -0,0 +1,16 @@
+/*
+Theme name: La Pizzaria
+Theme URI:
+Theme Author: Nathan Young
+Author URI:
+Discription: Theme for restaurants
+Version: 1.0
+License: GNU General Public Licence v2 or later
+License URI: http://www.gnu.org/license/glp-2.0.html
+Tags: Restaurant
+Text domain: lapizzaria
+*/
+
+<?php
+    echo "hello from my theme";
+?>
---
+++ wp-content/themes/pizzaria/page.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<section class="main-content">
+    <h1> Hello from page.php </h1>
+    <div class="test"></div>
+</section>
+
+<?php get_footer(); ?>
